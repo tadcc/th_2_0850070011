50dffa24e210d62a96984c7f6612869209b4922d
tadcc <tadc1107@gmail.com>
2023-08-03 22:38:54 +0700

    first commit

---
+++ cau 2/lib/screens/home/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:onlineshop/constants.dart';
+import 'package:onlineshop/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: const Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            // By default our  icon color is white
+            // ignore: deprecated_member_use
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            // By default our  icon color is white
+            // ignore: deprecated_member_use
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        const SizedBox(width: kDefaultPaddin / 2)
+      ],
+    );
+  }
+}
